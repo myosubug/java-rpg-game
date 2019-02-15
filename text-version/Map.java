189ee9571d9e6fc86c6cd2c1017f53ddb6d1063f
Sean Oh <seanoh.dev@gmail.com>
2019-02-15 12:03:20 -0700

    Map.java is tested with main and works, few changes on display

--- text-version/Map.java
+++ text-version/Map.java
@@ -1,10 +1,10 @@
 public class Map{
-	private List[][] mapGrid;		//this will be the 2d array for any specific map screen
-	private List[][] overWorldCoords;	//this is the ordered pair that tells where a map screen should sit among other map screens
+	private String[][] mapGrid;		//this will be the 2d array for any specific map screen
+	private String[][] overWorldCoords;	//this is the ordered pair that tells where a map screen should sit among other map screens
 
 
 	//CONSTRUCTORS
-	public Map(ArrayList mapGrid, ArrayList overWorldCoords){
+	public Map(String[][] mapGrid, String[][] overWorldCoords){
 		this.mapGrid = mapGrid;
 		this.overWorldCoords = overWorldCoords;
 	}
@@ -13,17 +13,34 @@ public class Map{
 		this.mapGrid = createEmptyMap();
 	}
 
-
 	//METHODS
 	/**Note, all maps are 8x8*/
 
-	public createEmptyMap(){			//creates an 8x8 map with no enemies, items, or obstacles
+	public String[][] getMap(){
+		return this.mapGrid;
+	}
+
+	public String[][] createEmptyMap(){			//creates an 8x8 map with no enemies, items, or obstacles
 		String gameBoard[][] = new String[8][8];
 		for(int i = 0; i < gameBoard.length; i++){
 			for(int j = 0; j < gameBoard[i].length; j++){
-				gameBoard[i][j] = "-";
+				gameBoard[i][j] = " * ";
 			}
 		}
 		return gameBoard;
+	}
+
+	public static void main(String[] args) {
+		Map m = new Map();
+		String[][] d;
+		d = m.getMap();
+		for(int i = 0; i < d.length; i++){
+			System.out.println();
+			for(int j = 0; j < d[0].length; j++){
+				System.out.print(d[i][j]);
+			}
+		}
+		System.out.println();
+			
 	}
 }
