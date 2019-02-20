f3c60c53c4e8e04ba6720ff743bf5dcf57912a88
Emily Baird <emily.baird@ucalgary.ca>
2019-02-19 22:31:12 -0700

    fixed battle so 0 still quits. flow control for battle still broken

--- text-version/TextApp.java
+++ text-version/TextApp.java
@@ -14,7 +14,7 @@ public class TextApp extends Map{
 		This way we don't have to hard code the locations of each thing within the actual app
 
 		Sean: that's good idea, I will try to move everything to Map calss and let TextApp.java to just load currentMap!
-	
+
 			I have extended textapp.java with Map.java so that we can access all the thing we put on the map.
 		*/
 
@@ -37,7 +37,7 @@ public class TextApp extends Map{
 				if ((i == super.getPikachu().getX()) && (j == super.getPikachu().getY()))		//Prints P for Pikachu in char. location
 					System.out.print(" P ");
 				else 								//Otherwise prints map data
-					System.out.print(temp[i][j]);		
+					System.out.print(temp[i][j]);
 			}
 		}
 
@@ -65,8 +65,8 @@ public class TextApp extends Map{
 
 		test.printToConsole();
 		System.out.println();
-		//updated movement 
-		System.out.println("You can go: Left(4) Right(6) Up(8) Down(2)");
+		//updated movement
+		System.out.println("Use WASD to move");
 		System.out.println("Press 0 to quit the game");
 		System.out.println();
 
@@ -75,79 +75,92 @@ public class TextApp extends Map{
 		while (game == true){
 
 			//Takes user input for movement
-			int input = keyboard.nextInt();
-			if ((input == 2) || (input == 4) || (input == 6) || (input == 8)){
+			String input = keyboard.nextLine();
+			if ((input.equals("w")) || (input.equals("a")) || (input.equals("d")) || (input.equals("s"))){
 				test.getPikachu().move(input);
 				test.printToConsole();
-				System.out.println("You can go: Left(4) Right(6) Up(8) Down(2)");
+				System.out.println("Use WASD to move");
 				System.out.println("Press 0 to quit the game");
 				System.out.println();
 			}
 
-			else if (input == 0) 		//quits if user presses 0
+			else if (input == "0") 		//quits if user presses 0
 				game = false;
-			
+
 			// have added instructions for inputs rather than 2,4,6,8
 			else {
-				System.out.println("Please enter correct input for the movement,\nyou can go: Left(4) Right(6) Up(8) Down(2).");
+				System.out.println("Please enter correct input for the movement,\nyou can go: Left(a) Right(d) Up(w) Down(s).");
 			}
 
-			
+
 			//interaction starts when player meets something else rather than " - " in the map.
 			tempMap = test.getCurrentMap().getMap(); //getting copy of map
 			mapObject = tempMap[test.getPikachu().getX()][test.getPikachu().getY()]; //saving comparison string for interaction
 			double itemRate = Math.random();
-			
+
+			//object interaction
 			if (mapObject == " I "){
 				System.out.println("Found an item!, let's keep this in my bag! \n\nCurrent list of the items in the inventory: ");
-				
+
 				if (itemRate > 0.50)
 					test.getPikachu().addItemToInventory(test.getHP());
 				else
 					test.getPikachu().addItemToInventory(test.getBattleFruit());
-				
+
 				test.getPikachu().displayInventory();
 				System.out.println();
-				System.out.println("You can go: Left(4) Right(6) Up(8) Down(2)");
+				System.out.println("You can go: Left(a) Right(d) Up(w) Down(s)");
 				System.out.println();
 				continue;
-			
-			} else if (mapObject == " M "){
-				System.out.println("Monster is near! \nWhat do you want to do? : Fight(f/F) or Run away(r/R)");
-					char input2 = 'a';
-					char input3 = 'b';
-					
-				
-				while (input3 != 'F' || input3 != 'R'){
+
+			}
+
+			//Battle interaction
+			else if (mapObject == " M "){
+				System.out.println("Monster is near! \nWhat do you want to do? : Fight(f) or Run away(r)");
+
+					String input2 = keyboard.nextLine();
+
+
+			/*	while (input3 != 'F' || input3 != 'R'){
 					System.out.println();
 					input2 = keyboard.next().charAt(0);
 					input3 = Character.toUpperCase(input2);
-					System.out.println();
-					
-					if (input3 == 'F'){
+					System.out.println(); */
+
+					if (input2.equals("f")){
 						System.out.println("Preparing battle...");
 						System.out.println();
 						//now we need fight interaction here
+
+						mapObject = " - ";	//removes monster from board
 						break;
-					} else if (input3 == 'R'){
+					}
+
+					else if (input2.equals("r")){
 						System.out.println("You are safe now!");
 						System.out.println();
-						System.out.println("You can go: Left(4) Right(6) Up(8) Down(2)");
+						System.out.println("You can go: Left(a) Right(d) Up(w) Down(s)");
 						System.out.println("Press 0 to quit the game");
 						System.out.println();
+
+						mapObject = " - ";		//removes monster from board
 						break;
-					} else{
-						System.out.println("Please enter correct input for the movement,\nyou can Fight(f/F) or Run away(r/R)");
+					}
+
+					else if (input2.equals("0")){
+						game = false;
+					}
+
+					else{
+						System.out.println("Please enter correct input for the movement,\nyou can Fight(f) or Run away(r)");
 						System.out.println();
 						continue;
 					}
-				} 
 
-			} else {
-				continue;
+					continue;
+
 			}
-			
 		}
-		
 	}
 }
