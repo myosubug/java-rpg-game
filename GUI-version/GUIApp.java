bc9c7b611eefe9d366f4c3efba7e375cb123683b
Sean Oh <seanoh.dev@gmail.com>
2019-03-02 17:05:33 -0700

    organized code and added comments for other to understand

--- GUI-version/GUIApp.java
+++ GUI-version/GUIApp.java
@@ -44,70 +44,25 @@ public class GUIApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-
+        //setting up main layout and stage
         primaryStage.setTitle("Liberate Pikachu!");
         VBox root = new VBox();
         Scene theScene = new Scene(root, 400, 580);
         primaryStage.setScene(theScene);
-        
-        
-        
 
-        
+        //setting up canvas, which will be out game display area
         final Canvas canvas = new Canvas(400,400);
         root.getChildren().add(canvas);
 
         
 
-        /** 
-         * this is good for when key press and release needs to change its status
-        theScene.setOnKeyPressed(
-            new EventHandler<KeyEvent>()
-            {
-                public void handle(KeyEvent e)
-                {
-                    String code = e.getCode().toString().toUpperCase();
-                    if(!userInput.contains(code))
-                        userInput.add(code);    
-                }
-            });
-        
-        theScene.setOnKeyReleased(
-            new EventHandler<KeyEvent>()
-            {
-                public void handle(KeyEvent e){
-                    String code = e.getCode().toString().toUpperCase();
-                    userInput.remove(code);
-                }
-            });
-
-        */
-
+        //importing image from local directory and add it to canvas
         gc = canvas.getGraphicsContext2D();
-        
-        
         Image image = new Image("file:img/pikachu.gif");
         gc.drawImage(image, pikachuX, pikachuY);
-        //Sprite pikachu = new Sprite();
-        
-        //gc.drawImage(image, 0, 0);
-            
-        new AnimationTimer(){
-        
-            @Override
-            public void handle(long now) {
-                gc.clearRect(0, 0, 400, 400);
-                gc.drawImage(image, pikachuX, pikachuY);
-            }
-        }.start();
-    
 
-
-        
-
-        //setting up gridpane, root
-
-
+      
+       //setting up a borderpane to place status message section as label object 
        BorderPane root2 = new BorderPane();
        Label output = new Label("\nUse arrow keys or WASD to move around. \nAction buttons will be used to make choices in game.\n");
        Label filler = new Label("");
@@ -116,11 +71,10 @@ public class GUIApp extends Application {
        output.setMinWidth(400);
        output.setMaxHeight(100);
        output.setStyle("-fx-border-color: black;");
-       //output.setText("         this better work");
        root2.setTop(output);
        
       
-
+       //setting up buttons up/down/left/right and two action buttons
        GridPane moveButtons = new GridPane();
        Button btnLeft = new Button("←");
        Button btnRight = new Button("→");
@@ -137,7 +91,22 @@ public class GUIApp extends Application {
        moveButtons.add(btnRight,2, 1);
        moveButtons.add(btnDown,1, 2);
 
-       btnUp.setOnAction((e) -> {
+
+       //action button layout setting up
+       GridPane actionButtons = new GridPane();
+       Button btnA = new Button("J");
+       Button btnB = new Button("K");
+
+       btnA.setMinWidth(60);
+       btnB.setMinWidth(60);
+ 
+       actionButtons.add(btnA, 0, 1);
+       actionButtons.add(btnB, 2, 1);
+
+
+    //adding button events, two action buttons' action will be added below of this tooo
+    //the first 4 of these are button actions
+    btnUp.setOnAction((e) -> {
         if(pikachuY - 40 >= 0 && pikachuY - 40 <= 400)
             pikachuY -= 40;
         else
@@ -165,81 +134,75 @@ public class GUIApp extends Application {
             ;
     });
 
-
+    //these 4 are for keyboard action that is pressed as w/a/s/d
     theScene.setOnKeyPressed(
-            new EventHandler<KeyEvent>()
+        new EventHandler<KeyEvent>()
+        {
+        public void handle(KeyEvent e)
             {
-                public void handle(KeyEvent e)
-                {
-                    switch(e.getCode()){
-                        case W:
-                            if(pikachuY - 40 >= 0 && pikachuY - 40 <= 400){
-                                pikachuY -= 40;
-                                btnUp.requestFocus();
-                            }
-                            else
-                                ;
-                            break;
-                        case D:
-                            if(pikachuX + 40 >= 0 && pikachuX +40 <= 360){
-                                pikachuX += 40;
-                                btnRight.requestFocus();
-                            }
-                            else
-                                ;
-                            break;
-                        case S:
-                            if(pikachuY +40 >= 0 && pikachuY +40 <= +360){
-                                pikachuY += 40;
-                                btnDown.requestFocus();
-                            }
-                            else
-                                ;
-                            break;
-                        case A:
-                            if(pikachuX -40 >= 0 && pikachuX - 40 <= 400){
-                                pikachuX -= 40;
-                                btnLeft.requestFocus();
-                            }
-                            else
-                                ;
-                            break;
-                    }  
-                }
-            });
+            switch(e.getCode()){
+                    case W:
+                        if(pikachuY - 40 >= 0 && pikachuY - 40 <= 400){
+                            pikachuY -= 40;
+                            btnUp.requestFocus();
+                        }
+                        else
+                            ;
+                        break;
+                    case D:
+                        if(pikachuX + 40 >= 0 && pikachuX +40 <= 360){
+                            pikachuX += 40;
+                            btnRight.requestFocus();
+                        }
+                        else
+                            ;
+                        break;
+                    case S:
+                        if(pikachuY +40 >= 0 && pikachuY +40 <= +360){
+                            pikachuY += 40;
+                            btnDown.requestFocus();
+                        }
+                        else
+                            ;
+                        break;
+                    case A:
+                        if(pikachuX -40 >= 0 && pikachuX - 40 <= 400){
+                            pikachuX -= 40;
+                            btnLeft.requestFocus();
+                        }
+                        else
+                            ;
+                        break;
+                }  
+            }
+        });
 
 
-            
-         
-
-
-       GridPane actionButtons = new GridPane();
-       Button btnA = new Button("A");
-       Button btnB = new Button("B");
-
-       btnA.setMinWidth(60);
-       btnB.setMinWidth(60);
- 
-       actionButtons.add(btnA, 0, 1);
-       actionButtons.add(btnB, 2, 1);
-
-
-       root2.setLeft(moveButtons);
-       root2.setCenter(filler);
-       root2.setRight(actionButtons);
-       root.getChildren().add(root2);
-       
-      
-
-
-       primaryStage.show();
-
-
-        // ============== setting up the interface up to here ==============================
+    new AnimationTimer(){
         
+        @Override
+        public void handle(long now) {
+            gc.clearRect(0, 0, 400, 400);
+            gc.drawImage(image, pikachuX, pikachuY);
+        }
+    }.start();
+
+
+
+    root2.setLeft(moveButtons);
+    root2.setCenter(filler);
+    root2.setRight(actionButtons);
+    root.getChildren().add(root2);
+    
+    primaryStage.show();
+
+    // ============== setting up the interface up to here ==============================
     }
 
+     
+
     /**
+     * THESE ARE EXTRA CODES THAT MIGHT BE USEFUL LATER TO I SAVED THEM UNDER HERE
      * 
      
     public static void keyboardAction(){
@@ -275,7 +238,7 @@ public class GUIApp extends Application {
         }
     }
 
+    
     */
-
  
 }
