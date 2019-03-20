160fe4c0e813740658738250b2ed176f7078137f
myosubug <seanoh.dev@gmail.com>
2019-03-19 18:11:18 -0600

    added try-catch for collision check methods

--- GUI-version/Collision.java
+++ GUI-version/Collision.java
@@ -5,38 +5,58 @@ public class Collision {
     public boolean objectCollisionCheck (int pikachuX, int pikachuY, char[][] mapData){
         int pikachuXDivided = pikachuX / 32;
         int pikachuYDivided = pikachuY / 32;
+        try{
         if (mapData[pikachuYDivided][pikachuXDivided] == 'x')
             return true;
         else
             return false;
+        }catch(Exception e){
+            System.out.println("Object Collision checking ...");
+            return false;
+        }
     }
 
     public boolean miniBossCollisionCheck (int pikachuX, int pikachuY, char[][] mapData){
         int pikachuXDivided = pikachuX / 32;
         int pikachuYDivided = pikachuY / 32;
+        try{
         if (mapData[pikachuYDivided][pikachuXDivided] == '*')
             return true;
         else
             return false;
+        } catch(Exception e){
+            System.out.println("Object Collision checking ...");
+            return false;
+        }
     }
 
 
     public boolean secondMapUpdateCheck(int pikachuX, int pikachuY, char[][] mapData){
         int pikachuXDivided = pikachuX / 32;
         int pikachuYDivided = pikachuY / 32;
+        try{
         if (mapData[pikachuYDivided][pikachuXDivided] == '#')
             return true;
         else
             return false;
+        } catch(Exception e){
+            System.out.println("Object Collision checking ...");
+            return false;
+        }
     }
 
     public boolean firstMapUpdateCheck(int pikachuX, int pikachuY, char[][] mapData){
         int pikachuXDivided = pikachuX / 32;
         int pikachuYDivided = pikachuY / 32;
+        try{
         if (mapData[pikachuYDivided][pikachuXDivided] == '@')
             return true;
         else
             return false;
+        } catch(Exception e){
+            System.out.println("Object Collision checking ...");
+            return false;
+        }
     }
     
 }
