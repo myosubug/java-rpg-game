8c9e3e6ab4cf8d9a6143d50b78e4c8fc619096ac
Emily Baird <emily.baird@ucalgary.ca>
2019-03-20 12:16:07 -0600

    testing save functionality

--- GUI-version/Test.java
+++ GUI-version/Test.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.awt.event.ItemListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.concurrent.ThreadLocalRandom;
+import java.io.*;
 
-public class Test{
+public class Test implements Serializable{
   private char[][] mapData = new char[20][20];
 	private String mapFileName = "map1.txt";
 
+  //methods
   public static char[][] readMapFile(String fileName) throws FileNotFoundException{
     try{
       int lineWidth = 20;
@@ -31,14 +30,45 @@ public class Test{
     }
   }
 
+  //testing save functionality
+  /*public void serializeMap() throws IOException{
+    try{
+      FileOutputStream fileOut = new FileOutputStream("/temp/testMap.ser");
+      ObjectOutputStream mapOut = new ObjectOutputStream(fileOut);
+      mapOut.writeObject(this);
+      mapOut.close();
+      fileOut.close();
+    }
+
+    catch(IOException i){
+      i.printStackTrace();
+    }
+  }*/
+
+  //main method
   public static void main(String[] args) {
     char[][] testMapData = new char[20][20];
     try{
       testMapData = readMapFile("mapData/map1.txt");
+
+      //testing save functionality
+      try{
+        FileOutputStream fileOut = new FileOutputStream("testMap.ser");
+        ObjectOutputStream mapOut = new ObjectOutputStream(fileOut);
+        mapOut.writeObject(testMapData);
+        mapOut.close();
+        fileOut.close();
+      }
+
+      catch(IOException i){
+        i.printStackTrace();
+      }
+
     }
 
     //if it's unable to load file, will create a map with no obstacles instead
     catch(FileNotFoundException ex){
+      System.out.println("Could not load file, blank map created instead.");
       for (int i = 0; i < 20; i++){
         for (int j = 0; j < 20; j++){
           String dash = "-";
