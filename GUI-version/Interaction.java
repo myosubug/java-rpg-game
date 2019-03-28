856d8d247259c5631688053b8040058f4690623e
sungjong.oh <sungjong.oh@cs.ucalgary.ca>
2019-03-28 16:18:51 -0600

    Revert "changes to interaction class to be able to implement Boss"
    This reverts commit de58c035e9648fcb8d9a2058f41a3564f7d74595

--- GUI-version/Interaction.java
+++ GUI-version/Interaction.java
@@ -10,24 +10,21 @@ public class Interaction {
       * @param battleOutput this is label that is in battle scene.
       * @return true is battle is finished and false if battle is not finished.
       */
-    public boolean battle(Player pikachu, Creature monster, Label battleOutput){
+    public boolean battle(Player pikachu, Creature monster, Label battleOutput){      
         double fightRate = Math.random();
         //by 59% of chance, monster attacks back player every time player tries to attack the monster
         if(fightRate < 0.60){
             monster.setHP(-pikachu.getAttack());
-            battleOutput.setText("Your attack was succesful! \nYou did " + pikachu.getAttack() + " damage!");
+            battleOutput.setText("Your attack was succesful!");
         }
         else{
             pikachu.setHP(-monster.getAttack());
-            battleOutput.setText("You tried to attack, but " + monster.getName() + " attacked you back for " + monster.getAttack() + " damage.");
+            battleOutput.setText(monster.getName()+" attacked you back!");
         }
-
-
-
 
         //if either of monster or player's hp become 0, the battle is over.
         if (pikachu.getHP() <= 0){
-            battleOutput.setText("============================================================\n===========YOU HAVE LOST THE BATTLE!==========================\n===========PLEASE GO BACK TO GAME MAP BY PRESSING Q===============\n============================================================");
+            battleOutput.setText("============================================================\n===========YOU HAVE LOST THE BATTLE!==========================\n===========PLEASE GO BACK TO GAME MAP BY PRESS Q===============\n============================================================");
             pikachu.setHP(1);
             return true;
         } else if(monster.getHP() <= 0){
