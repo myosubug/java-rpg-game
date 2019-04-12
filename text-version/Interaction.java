25c7e28f231df46f9388665a8d6ef1bf8deb06fe
myosubug <seanoh.dev@gmail.com>
2019-04-11 19:28:35 -0600

    recovering files

--- text-version/Interaction.java
+++ text-version/Interaction.java
@@ -40,9 +40,6 @@ public class Interaction extends Creature{
                 System.out.println();
                 System.out.println(this.monster.toString());
 
-                if (this.monster.getHP() <= 0)
-                    break;
-
                 //the monster also have chance to attack the player back by 50%
                 if(monsterAttackChance > 0.50){
                     System.out.println();
